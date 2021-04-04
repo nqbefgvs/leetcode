624f3ff224fbcf81947b3f4e2e295160d8c77e73
nqbefgvs <389179622@qq.com>
2021-04-04 20:27:43 +0800

    循环队列的实现,目前想到两种方式：一种是数组大小刚好等于构造函数传参值，用额外的标志位表示循环队列是满还是空；另一种是数组大小比构造函数传参值要大，头尾指针在队列满和队列空时候的相对位置就不会相同。

---
+++ src/MyCircularQueue.java
@@ -0,0 +1,72 @@
+public class MyCircularQueue {
+
+    int[] q;
+    int front;
+    int len;
+
+    public MyCircularQueue(int k) {
+        q = new int[k];
+        front = 0;
+        len = 0;
+    }
+
+    public boolean enQueue(int value) {
+        if(len < q.length){
+            q[(front + len++) % q.length] = value;
+            return true;
+        }
+        else return false;
+    }
+
+    public boolean deQueue() {
+        if(len > 0){
+            front = (front + 1) % q.length;
+            len--;
+            return true;
+        }
+        else return false;
+    }
+
+    public int Front() {
+        if(len > 0)
+            return q[front];
+        else return -1;
+    }
+
+    public int Rear() {
+        if(len > 0)
+            return q[(front + len - 1) % q.length];
+        else return -1;
+    }
+
+    public boolean isEmpty() {
+        return len == 0;
+    }
+
+    public boolean isFull() {
+        return len == q.length;
+    }
+
+    public static void main(String args[]){
+        MyCircularQueue m = new MyCircularQueue(3);
+        m.enQueue(1);
+        m.enQueue(2);
+        m.enQueue(3);
+        m.enQueue(4);
+        System.out.println(m.Rear());
+        m.deQueue();
+        m.enQueue(4);
+        System.out.println(m.Rear());
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
