3f2fb9327e563ec67f0ab60226644cc02e379f39
nqbefgvs <389179622@qq.com>
2021-04-12 16:43:58 +0800

    题目是“优美的排列”,我写的是优美的代码,对自己写的代码很满意

---
+++ src/BeautifulArrangementII.java
@@ -0,0 +1,22 @@
+public class BeautifulArrangementII {
+    public int[] constructArray(int n, int k) {
+        int[] res = new int[n];
+        int count = n;
+        while(--k > 0){
+            res[n - 1 - k--] = count--;
+        }
+        for(int i = n-1; i >= 0; i--){
+            if(res[i] == 0){
+                res[i] = count--;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args){
+        int[] res = new BeautifulArrangementII().constructArray(3, 2);
+        for(int i = 0; i < res.length; i++){
+            System.out.println(res[i]);
+        }
+    }
+}
