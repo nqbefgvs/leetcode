3de41c520a0d7a79a899a96e0a1c98d6dbf22c85
nqbefgvs <389179622@qq.com>
2021-04-18 19:35:48 +0800

    easy

---
+++ src/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,23 @@
+public class RemoveNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode cur = head;
+        int length = 0;
+        while(cur != null){
+            length++;
+            cur = cur.next;
+        }
+        ListNode prev = null;
+        cur = head;
+        length -= n;
+        if(length == 0){
+            return cur.next;
+        }
+        while(length > 0){
+            prev = cur;
+            cur = cur.next;
+            length--;
+        }
+        prev.next = cur.next;
+        return head;
+    }
+}
