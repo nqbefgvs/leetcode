50f70745f6efa7e52603f62bee271bc62de436a6
nqbefgvs <389179622@qq.com>
2021-04-23 16:18:31 +0800

    真就暴力啊，虽然不这样也做不出来，毕竟每个点都需要过一遍。

---
+++ src/BrickWall.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BrickWall {
+    int best = 0;
+    Map<Integer, Integer> freq = new HashMap<>();
+    public int leastBricks(List<List<Integer>> wall) {
+        for (int i = 0; i < wall.size(); i++)
+            processRow(wall.get(i));
+        return wall.size() - best;
+    }
+    public void processRow(List<Integer> row) {
+        int rowSum = row.get(0);
+        for (int j = 1; j < row.size(); j++) {
+            int f = freq.getOrDefault(rowSum, 0) + 1;
+            freq.put(rowSum, f);
+            if (f > best) best = f;
+            rowSum += row.get(j);
+        }
+    }
+}
