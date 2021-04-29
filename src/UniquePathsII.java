b74dd55108f99e35ddd10bdef2d19dad1cb2ddf5
nqbefgvs <389179622@qq.com>
2021-04-29 12:33:22 +0800

    简单的dp题

---
+++ src/UniquePathsII.java
@@ -0,0 +1,19 @@
+public class UniquePathsII {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        dp[0][0] = 1;
+        for(int i = 0; i < obstacleGrid.length; i++){
+            for(int j = 0; j < obstacleGrid[0].length; j++){
+                dp[i][j] += (i > 0 ? dp[i-1][j] : 0) + (j > 0 ? dp[i][j-1] : 0);
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1];
+    }
+
+    public static void main(String[] args){
+        new UniquePathsII().uniquePathsWithObstacles(new int[][]{{0,0,0},{0,1,0},{0,0,0}});
+    }
+}
