4808cc1c677083770aa98918b4d423c0f8b281cb
nqbefgvs <389179622@qq.com>
2021-06-10 19:03:03 +0800

    头一次看到treemap出现在算法题中

---
+++ src/MyCalendarI.java
@@ -0,0 +1,20 @@
+import java.util.TreeMap;
+
+public class MyCalendarI {
+    TreeMap<Integer, Integer> calendar;
+
+    MyCalendarI() {
+        calendar = new TreeMap();
+    }
+
+    public boolean book(int start, int end) {
+        Integer prev = calendar.floorKey(start),
+                next = calendar.ceilingKey(start);
+        if ((prev == null || calendar.get(prev) <= start) &&
+                (next == null || end <= next)) {
+            calendar.put(start, end);
+            return true;
+        }
+        return false;
+    }
+}
